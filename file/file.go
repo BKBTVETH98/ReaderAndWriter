221c2ecdc60ebc04bfaf76f5550e6fc394f2578b
bkbtveth <bkbtveth@mail.ru>
2026-02-16 08:11:46 +0300

    mv ReadWrite

---
+++ file/file.go
@@ -0,0 +1,39 @@
+package files
+
+import (
+	"fmt"
+	"os"
+)
+
+func WriteFile(content []byte) {
+
+	file, err := os.Create("account.json")
+
+	if err != nil {
+		fmt.Printf("Ошибка - %w", err)
+	}
+	defer file.Close()
+
+	_, err = file.Write(content)
+
+	if err != nil {
+		fmt.Printf("Ошибка - %w", err)
+		return
+	}
+
+	fmt.Println("Запись - ок")
+	fmt.Println()
+
+}
+
+func ReadFile(name string) ([]byte, error) {
+
+	readFile, err := os.ReadFile(name)
+
+	if err != nil {
+		fmt.Println("Ошибка чтения ", err)
+		return nil, err
+	}
+
+	return readFile, nil
+}
